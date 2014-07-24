c5f34f458c0fea5f6a80c24c0ffa86c102fe0a1d
Abraham Rubin <abrahamgrubin@gmail.com>
2014-07-24 12:47:36 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aviato::Application.config.secret_key_base = '8ae33472a7498be26770a8455e374eeef8ed9878f76e665a43ec69b04222919e0e22dc43aa5e3e13b8338a7ed3e7a76620c5bd0974083871840dd87482322fb0'
