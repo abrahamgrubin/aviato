d3260971fc53d1d27c50f18a4f8ba77de9b47a39
Abraham Rubin <abrahamgrubin@gmail.com>
2014-08-06 10:26:26 -0300

    Facebook configuration

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
+gem 'simple_hashtag'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Aviato::Application.routes.draw do
+  get "hashtags/:hashtag",   to: "hashtags#show",      as: :hashtag
+  get "hashtags",            to: "hashtags#index",     as: :hashtags
   resources :welcome
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'welcome#index'
